617a61cb066c398991d52b7ccad07ab7e3dfc5d4
andrew <ayietaphilip00@outlook.com>
2020-08-11 18:07:32 +0300

    Input a function to go to the next question when the next button is clicked

--- js/script.js
+++ js/script.js
@@ -18,24 +18,24 @@ var questions = [{
     question: "5. What would following code return? console.log(typeof typeof 1);",
     choices: ["string", "number", "Syntax error", "undefined"],
     correctAnswer: 0
-},{
-	question: "6. Which software company developed JavaScript?",
+}, {
+    question: "6. Which software company developed JavaScript?",
     choices: ["Mozilla", "Netscape", "Sun Microsystems", "Oracle"],
     correctAnswer: 1
-},{
-	question: "7. What would be the result of 3+2+'7'?",
+}, {
+    question: "7. What would be the result of 3+2+'7'?",
     choices: ["327", "12", "14", "57"],
     correctAnswer: 3
-},{
-	question: "8. Look at the following selector: $('div'). What does it select?",
+}, {
+    question: "8. Look at the following selector: $('div'). What does it select?",
     choices: ["The first div element", "The last div element", "All div elements", "Current div element"],
     correctAnswer: 2
-},{
-	question: "9. How can a value be appended to an array?",
+}, {
+    question: "9. How can a value be appended to an array?",
     choices: ["arr(length).value;", "arr[arr.length]=value;", "arr[]=add(value);", "None of these"],
     correctAnswer: 1
-},{
-	question: "10. What will the code below output to the console? console.log(1 +  +'2' + '2');",
+}, {
+    question: "10. What will the code below output to the console? console.log(1 +  +'2' + '2');",
     choices: ["'32'", "'122'", "'13'", "'14'"],
     correctAnswer: 0
 }];
@@ -44,24 +44,45 @@ var viewingAns = 0;
 var correctAnswers = 0;
 var quizOver = false;
 var iSelectedAnswer = [];
-	var c=180;
-	var t;
-$(document).ready(function () 
-{
+var c = 180;
+var t;
+$(document).ready(function () {
     // Display the first question
     displayCurrentQuestion();
     $(this).find(".quizMessage").hide();
     $(this).find(".preButton").attr('disabled', 'disabled');
-	
-	timedCount();
-	
-	$(this).find(".preButton").on("click", function () 
-	{		
-		
-        if (!quizOver) 
-		{
-			if(currentQuestion == 0) { return false; }
-	
-			if(currentQuestion == 1) {
-			  $(".preButton").attr('disabled', 'disabled');
-			}
+
+    timedCount();
+
+    $(this).find(".preButton").on("click", function () {
+
+        if (!quizOver) {
+            if (currentQuestion == 0) { return false; }
+
+            if (currentQuestion == 1) {
+                $(".preButton").attr('disabled', 'disabled');
+            }
+            currentQuestion--; // Since we have already displayed the first question on DOM ready
+            if (currentQuestion < questions.length) {
+                displayCurrentQuestion();
+
+            }
+        } else {
+            if (viewingAns == 3) { return false; }
+            currentQuestion = 0; viewingAns = 3;
+            viewResults();
+        }
+    });
+
+
+    // On clicking next, display the next question
+    $(this).find(".nextButton").on("click", function () {
+        if (!quizOver) {
+
+            var val = $("input[type='radio']:checked").val();
+
+            if (val == undefined) {
+                $(document).find(".quizMessage").text("Please select an answer");
+                $(document).find(".quizMessage").show();
+            }
+            else
